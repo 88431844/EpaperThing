1e88e8c463224460d99c0eb892066598f49d29ec
zhhgao <zhhgao@mobvoi.com>
2019-01-16 16:28:55 +0800

    Create epaperThing.ino
    add main file

---
+++ epaperThing.ino
@@ -0,0 +1,71 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+/*Put your SSID & Password*/
+const char* ssid = "YourNetworkName";  // Enter SSID here
+const char* password = "YourPassword";  //Enter Password here
+
+ESP8266WebServer server(80);
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handle_OnConnect() {
+  server.send(200, "text/html", SendHTML(0,0)); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float Temperaturestat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>ESP8266 Weather Report</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>ESP8266 NodeMCU Weather Report</h1>\n";
+  
+  ptr +="<p>Temperature: ";
+  ptr +=(int)Temperaturestat;
+  ptr +="°C</p>";
+  ptr +="<p>Humidity: ";
+  ptr +=(int)Humiditystat;
+  ptr +="%</p>";
+  
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
