f3bc161825c67312b56b2b044bb6e9de922a5167
zhhgao <zhhgao@mobvoi.com>
2019-01-28 10:30:23 +0800

    del desc

--- epaperThing.ino
+++ epaperThing.ino
@@ -1,29 +1,3 @@
-/**
- *  @filename   :   epd2in9-demo.ino
- *  @brief      :   2.9inch e-paper display demo
- *  @author     :   Yehui from Waveshare
- *
- *  Copyright (C) Waveshare     September 9 2017
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documnetation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to  whom the Software is
- * furished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 #include <SPI.h>
 #include "epd2in9.h"
 #include "epdpaint.h"
@@ -31,11 +5,10 @@
 #include <Time.h>
 #include <Timezone.h>
 #include "NTP.h"
-//#define WIFI_SSID "Mobvoi-Guest"       // 使用时请修改为当前你的 wifi ssid
-//#define WIFI_PASS "mobvoiguest"   // 使用时请修改为当前你的 wifi 密码
+
 
 #define WIFI_SSID "HiWiFi_301"       // 使用时请修改为当前你的 wifi ssid
-#define WIFI_PASS "13520391363"   // 使用时请修改为当前你的 wifi 密码
+#define WIFI_PASS ""   // 使用时请修改为当前你的 wifi 密码
 
 //#define WIFI_SSID "luck max"       // 使用时请修改为当前你的 wifi ssid
 //#define WIFI_PASS "402050821"   // 使用时请修改为当前你的 wifi 密码
