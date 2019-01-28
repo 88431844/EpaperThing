8e872d66744d8452f889a6fb2b4e819b172454b5
zhhgao <zhhgao@mobvoi.com>
2019-01-28 10:11:27 +0800

    实现了 NTP读取网络时间 并在墨水屏上面显示时间，日期

--- epaperThing.ino
+++ epaperThing.ino
@@ -1,71 +1,242 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+/**
+ *  @filename   :   epd2in9-demo.ino
+ *  @brief      :   2.9inch e-paper display demo
+ *  @author     :   Yehui from Waveshare
+ *
+ *  Copyright (C) Waveshare     September 9 2017
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documnetation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to  whom the Software is
+ * furished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-/*Put your SSID & Password*/
-const char* ssid = "YourNetworkName";  // Enter SSID here
-const char* password = "YourPassword";  //Enter Password here
+#include <SPI.h>
+#include "epd2in9.h"
+#include "epdpaint.h"
+#include "imagedata.h"
+#include <Time.h>
+#include <Timezone.h>
+#include "NTP.h"
+//#define WIFI_SSID "Mobvoi-Guest"       // 使用时请修改为当前你的 wifi ssid
+//#define WIFI_PASS "mobvoiguest"   // 使用时请修改为当前你的 wifi 密码
 
-ESP8266WebServer server(80);
+#define WIFI_SSID "HiWiFi_301"       // 使用时请修改为当前你的 wifi ssid
+#define WIFI_PASS "13520391363"   // 使用时请修改为当前你的 wifi 密码
+
+//#define WIFI_SSID "luck max"       // 使用时请修改为当前你的 wifi ssid
+//#define WIFI_PASS "402050821"   // 使用时请修改为当前你的 wifi 密码
+// 北京时间时区
+#define STD_TIMEZONE_OFFSET +8    // Standard Time offset (-7 is mountain time)
+// Define daylight savings time rules for the China
+TimeChangeRule mySTD = {"", First,  Sun, Jan, 0, STD_TIMEZONE_OFFSET * 60};
+Timezone myTZ(mySTD, mySTD);
+
+WiFiClient client;
+
+#define COLORED     0
+#define UNCOLORED   1
+// Previous seconds value
+time_t previousSecond = 0;
+/**
+  * Due to RAM not enough in Arduino UNO, a frame buffer is not allowed.
+  * In this case, a smaller image buffer is allocated and you have to 
+  * update a partial display several times.
+  * 1 byte = 8 pixels, therefore you have to set 8*N pixels at a time.
+  */
+unsigned char image[1024];
+Paint paint(image, 0, 0);    // width should be the multiple of 8 
+Epd epd;
+unsigned long time_start_ms;
+unsigned long time_now_s;
+
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  delay(100);
-  
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-
-  //connect to your local wi-fi network
-  WiFi.begin(ssid, password);
-
-  //check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
+  if (epd.Init(lut_full_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
   }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+initNTP(WIFI_SSID, WIFI_PASS);
+  /** 
+   *  there are 2 memory areas embedded in the e-paper display
+   *  and once the display is refreshed, the memory area will be auto-toggled,
+   *  i.e. the next action of SetFrameMemory will set the other memory area
+   *  therefore you have to clear the frame memory twice.
+   */
+  epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayFrame();
+  epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayFrame();
 
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
+//  paint.SetRotate(ROTATE_0);
+//  paint.SetWidth(128);
+//  paint.SetHeight(24);
+//
+//  /* For simplicity, the arguments are explicit numerical coordinates */
+//  paint.Clear(COLORED);
+//  paint.DrawStringAt(0, 4, "Hello world!", &Font16, UNCOLORED);
+//  epd.SetFrameMemory(paint.GetImage(), 0, 10, paint.GetWidth(), paint.GetHeight());
+//  
+//  paint.Clear(UNCOLORED);
+//  paint.DrawStringAt(0, 4, "e-Paper Demo", &Font16, COLORED);
+//  epd.SetFrameMemory(paint.GetImage(), 0, 30, paint.GetWidth(), paint.GetHeight());
+//
+//  paint.SetWidth(64);
+//  paint.SetHeight(64);
+//  
+//  paint.Clear(UNCOLORED);
+//  paint.DrawRectangle(0, 0, 40, 50, COLORED);
+//  paint.DrawLine(0, 0, 40, 50, COLORED);
+//  paint.DrawLine(40, 0, 0, 50, COLORED);
+//  epd.SetFrameMemory(paint.GetImage(), 16, 60, paint.GetWidth(), paint.GetHeight());
+//
+//  paint.Clear(UNCOLORED);
+//  paint.DrawCircle(32, 32, 30, COLORED);
+//  epd.SetFrameMemory(paint.GetImage(), 72, 60, paint.GetWidth(), paint.GetHeight());
+//
+//  paint.Clear(UNCOLORED);
+//  paint.DrawFilledRectangle(0, 0, 40, 50, COLORED);
+//  epd.SetFrameMemory(paint.GetImage(), 16, 130, paint.GetWidth(), paint.GetHeight());
+//
+//  paint.Clear(UNCOLORED);
+//  paint.DrawFilledCircle(32, 32, 30, COLORED);
+//  epd.SetFrameMemory(paint.GetImage(), 72, 130, paint.GetWidth(), paint.GetHeight());
+//  epd.DisplayFrame();
+//
+//  delay(2000);
 
-  server.begin();
-  Serial.println("HTTP server started");
+  if (epd.Init(lut_partial_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
+  }
+
+  /** 
+   *  there are 2 memory areas embedded in the e-paper display
+   *  and once the display is refreshed, the memory area will be auto-toggled,
+   *  i.e. the next action of SetFrameMemory will set the other memory area
+   *  therefore you have to set the frame memory and refresh the display twice.
+   */
+//  epd.SetFrameMemory(IMAGE_DATA);
+//  epd.DisplayFrame();
+//  epd.SetFrameMemory(IMAGE_DATA);
+//  epd.DisplayFrame();
+
+  time_start_ms = millis();
 }
 
 void loop() {
-  server.handleClient();
-}
 
-void handle_OnConnect() {
-  server.send(200, "text/html", SendHTML(0,0)); 
-}
+ //  Update the display only if time has changed
+  if (timeStatus() != timeNotSet) {
+    if (second() != previousSecond) {
+      previousSecond = second();
+      // Update the display
+      updateDisplay();
+    }
+  }
+  delay(500);
 
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(float Temperaturestat,float Humiditystat){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>ESP8266 Weather Report</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<div id=\"webpage\">\n";
-  ptr +="<h1>ESP8266 NodeMCU Weather Report</h1>\n";
   
-  ptr +="<p>Temperature: ";
-  ptr +=(int)Temperaturestat;
-  ptr +="°C</p>";
-  ptr +="<p>Humidity: ";
-  ptr +=(int)Humiditystat;
-  ptr +="%</p>";
   
-  ptr +="</div>\n";
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
+}
+
+void updateDisplay(void) {
+
+  TimeChangeRule *tcr;        // Pointer to the time change rule
+
+  // Read the current UTC time from the NTP provider
+  time_t utc = now();
+
+  // Convert to local time taking DST into consideration
+  time_t localTime = myTZ.toLocal(utc, &tcr);
+
+  // Map time to pixel positions
+  int weekdays=   weekday(localTime);
+  int days    =   day(localTime);
+  int months  =   month(localTime);
+  int years   =   year(localTime);
+  int seconds =   second(localTime);
+  int minutes =   minute(localTime);
+  int hours   =   hour(localTime) ;   //12 hour format use : hourFormat12(localTime)  isPM()/isAM()
+  Serial.println("");
+  Serial.print("Current local time:");
+  Serial.print(days);
+  Serial.print("/");
+  Serial.print(months);
+  Serial.print("/");
+  Serial.print(years);
+  Serial.print(" - ");
+  Serial.print(hours);
+  Serial.print(":");
+  Serial.print(minutes);
+  Serial.print(":");
+  Serial.print(seconds);
+  Serial.print(" - ");
+  Serial.print(dayStr(weekdays));
+  String h = "";
+  String m = "";
+  String s = "";
+  String w = dayStr(weekdays);
+  if(hours < 10 ){
+    h = "0"+String(hours);
+  }else{
+    h = String(hours);
+  }
+    if(minutes < 10 ){
+    m = "0"+String(minutes);
+  }
+  else{
+    m = String(minutes);
+  }
+    if(seconds < 10 ){
+    s = "0"+String(seconds);
+  }else{
+    s = String(seconds);
+  }
+  String mytime = h + " : " + m ;
+  String mydate = String(years) + "/" + months + "/" + days + "" + w;
+  Serial.println("!!!: " + mytime);
+  Serial.println("!!!: " + mydate);
+
+//
+  paint.SetWidth(32);
+  paint.SetHeight(188);
+  paint.SetRotate(ROTATE_90);
+//
+
+  paint.Clear(UNCOLORED);
+char __mytime[sizeof(mytime)];
+    mytime.toCharArray(__mytime, sizeof(__mytime));
+  paint.DrawStringAt(0, 4,__mytime , &Font24, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 80, 72, paint.GetWidth(), paint.GetHeight());
+
+ paint.Clear(UNCOLORED);
+char __mydate[sizeof(mydate)];
+    mydate.toCharArray(__mydate, sizeof(__mydate));
+  paint.DrawStringAt(0, 4, __mydate, &Font24, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 0, 50, paint.GetWidth(), paint.GetHeight());
+
+
+  
+
+//  paint.DrawStringAt(0, 4, "e-Paper Demo", &Font16, COLORED);
+//  epd.SetFrameMemory(paint.GetImage(), 0, 30, paint.GetWidth(), paint.GetHeight());
+  
+  epd.DisplayFrame();
+//
+  delay(500);
 }
